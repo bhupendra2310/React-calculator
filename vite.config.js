654a09f0b759fcd074fb6d007f2193d0ea977469
Bhupendra Kushwah <bhupenkushwah23@gmail.com>
2025-04-18 18:42:31 +0530

    add build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-calculator/', // This is where you specify your repo name (base path)
   plugins: [react()],
 })
